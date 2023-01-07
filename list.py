581cb2846e1c0dba8fba9f359e256d7c19cbf698
alikatgh <i@alikn.ru>
2023-01-07 15:50:11 +0600

    iterate over list

--- list.py
+++ list.py
@@ -43,3 +43,13 @@ matrix = [[col for col in range(5)] for row in range(5)]
 
 cond = [round(num ** 0.33, 2) if num % 1 == 0 else 0 for num in range(20)]
 print(cond)
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry',
+              'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member for member in fellowship if len(member) >= 7]
+
+# Print the new list
+print(new_fellowship)
