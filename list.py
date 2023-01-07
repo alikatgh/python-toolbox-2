e503aedeccaaa126c779958c129ba575588154ed
alikatgh <i@alikn.ru>
2023-01-07 16:24:24 +0600

    iterate over list part II

--- list.py
+++ list.py
@@ -59,3 +59,13 @@ new_fellowship = [member if len(member) >= 7 else '' for member in fellowship]
 
 # Print the new list
 print(new_fellowship)
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry',
+              'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create dict comprehension: new_fellowship
+new_fellowship = {member: len(member) for member in fellowship}
+
+# Print the new dictionary
+print(new_fellowship)
