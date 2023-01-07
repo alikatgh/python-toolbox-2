3db525e9980144fbf6e333fce449e7e6e5027b7e
alikatgh <i@alikn.ru>
2023-01-07 15:58:05 +0600

    iterate over list part II

--- list.py
+++ list.py
@@ -53,3 +53,9 @@ new_fellowship = [member for member in fellowship if len(member) >= 7]
 
 # Print the new list
 print(new_fellowship)
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member if len(member) >= 7 else '' for member in fellowship]
+
+# Print the new list
+print(new_fellowship)
