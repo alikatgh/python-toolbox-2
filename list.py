b859966c4bad532b5311afea30ae428f8e6ebf72
alikatgh <i@alikn.ru>
2023-01-07 14:41:32 +0600

    creating matrix using list comprehension

--- list.py
+++ list.py
@@ -30,3 +30,13 @@ doctor = ['house', 'cuddy', 'chase', 'thirteen', 'wilson']
 #     for doc in doctor:
 #         doc[0]
 #     return
+
+# Create list comprehension: squares
+squares = [i ** 2 for i in range(0, 10)]
+
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
