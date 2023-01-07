aa314506cbe22e9b4070751378dc72a8910b677e
alikatgh <i@alikn.ru>
2023-01-07 16:33:08 +0600

    list to generators

--- list_to_gen.py
+++ list_to_gen.py
@@ -5,6 +5,15 @@ f2 = (2 * num for num in range(5))
 # print(type(f2))
 
 res = (2 * num for num in range(5))
-for i in res:
-    print(i)
-    print(type(i))
+# print(next(res))
+# print(next(res))
+# print(next(res))
+# print(next(res))
+# print(next(res))
+
+res2 = (2 * num for num in range(5))
+for i in res2:
+    print((i))
+    # print(type(i))
+
+# print(next(res))
